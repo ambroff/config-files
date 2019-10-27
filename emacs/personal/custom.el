b995bd992acdd60123cfcc5014881d437cc793ef
Kyle Ambroff-Kao <kyle@ambroffkao.com>
2019-10-27 00:38:09 -0700

    I switched to lsp and I'm never going back.

--- emacs/personal/custom.el
+++ emacs/personal/custom.el
@@ -3,11 +3,39 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-enabled-themes (quote (wombat)))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (json-mode protobuf-mode rtags company-irony irony irony-eldoc cmake-mode yaml-mode markdown-mode zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring beacon anzu ace-window)))
- '(rtags-completing-read-behavior (quote insert-default))
- '(rtags-verify-protocol-version nil))
+    (use-package lsp-ui yasnippet company-lsp lsp-mode cmake-mode exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring beacon anzu ace-window)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -15,32 +43,21 @@
  ;; If there is more than one, they won't work right.
  )
 
-(setq prelude-guru nil)
-
 ;; C++ dev setup
 (c-set-offset 'innamespace 0)
 
 (eval-after-load 'company
-  '(add-to-list 'company-backends 'company-irony))
+  '(add-to-list 'company-backends 'company-lsp))
 
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-(add-hook 'irony-mode-hook #'irony-eldoc)
-(add-hook 'irony-mode-hook 'company-mode)
+(use-package lsp-mode
+  :hook (c++-mode . lsp)
+  :hook (c-mode . lsp)
+  :commands lsp
+  :config
+  (setq lsp-clients-clangd-args '("-j=2" "-background-index" "-log=error")))
 
-;; (set rtags-verify-protocol-version nil)
-;; (require 'rtags)
-;; (rtags-start-process-unless-running)
-;; (define-key c-mode-base-map (kbd "M-.")
-;;   (function rtags-find-symbol-at-point))
-;; (define-key c-mode-base-map (kbd "M-,")
-;;   (function rtags-find-references-at-point))
-
-;; (defun rtags-diagnostics (&optional restart nodirty)
-;;   (interactive "P")
-;;   (message "rtags-diagnostics has scalability issues and has been disabled.")
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package company-lsp :commands company-lsp)
 
 (company-mode t)
 (ido-mode t)
